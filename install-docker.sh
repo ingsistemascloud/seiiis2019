ba57bda22e7d310ae08002341b7b0fb69680e3e7
Ubuntu <ejemplo@ejemplo.nqvvg1g4o3depayaaapl4f0nid.bx.internal.cloudapp.net>
2019-10-14 17:01:06 +0000

    Script instalación simple de docker para aprendizaje y pruebas

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt-get update
+sudo wget -qO- https://get.docker.com/ | sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
